bacb68eecd1f767192fd61370f38939108d78ca1
Monica Nguyen <Monica.Nguyen@tesseractsensors.com>
2017-08-29 16:17:12 -0400

    FML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,7 @@
   var player1 = database.ref("p1Game");
   var player2 = database.ref("p2Game");
   var currentGame = database.ref("currentGame");
+  var me;
 
   //initialize Values
   var playerName = "";
@@ -24,7 +25,10 @@
   var p2win = 0;
   var p2lose = 0;
   var playerNum = 0;
+  var choice = "";
   var opponentChoice = "";
+  
+
 
 //-------------------------------------------------------------
 
@@ -38,8 +42,17 @@
     playerCount++;
     console.log("playerCount", playerCount);
 
+    //after click disable button
+    $("#startGame").prop("disabled", true);
+
+    //if playerCount is 1 then 
     if(playerCount == 1) {
+      //set playerNum to 1
       playerNum = "1";
+      //set variable me to p1Game
+      me = database.ref("p1Game");
+      console.log("p1", me);
+      //set database object player1
       player1.set({
         playerName: playerName,
         losses: 0,
@@ -47,13 +60,17 @@
         playerCount: playerNum,
         choice: "",
       });
+      //set numberOfPlayers to current playerCount to reference later
       currentGame.set({
         numOfPlayers: playerCount,
       });
+      $("#startGame").prop("disabled", true);
     }
     else if(playerCount == 2) {
       playerNum = "2";
       $("#startGame").prop("disabled", true);
+      me = database.ref("p2Game");
+      console.log("p2", me);
       player2.set({
         playerName: playerName,
         losses: 0,
@@ -87,67 +104,64 @@
     }
   });  
 
+  //listener for when player1 value changes
   player1.on("value", function(snapshot) {
+    //if value exists in playerName
     if(snapshot.child("playerName").exists()) {
+      //display player1 name
       $("#p1Player").html("Player 1: " + playerName);
+      //$("#p2Player").html("Player 2: " + player2.playerName);
+      console.log(player2.playerName);
+      //display waitingForPlayer function with parameter playerNum, which is 1
+      waitingForPlayer("player2", playerCount);
     }
+    //if value exists in losses
     if(snapshot.child("losses").exists()) {
+      //diplay player1 losses
       $("#p1Lose").html(snapshot.val().losses);
+      //database tells p1Lose value of losses
+      p1Lose = snapshot.val().losses;
+      console.log("p1 losses", p1Lose);
     }
     if(snapshot.child("wins").exists()) {
       $("#p1Wins").html(snapshot.val().wins);
+      //database tells p1Wins value of wins
+      p1Wins = snapshot.val().wins;
+      console.log("p1 wins", p1Wins);
     }
     // if(snapshot.child("choice").exists()) {
-    //   $("#p1Choice").html(snapshot.val().losses);
+    //   $("#p1Choice").html(snapshot.val().choice);
     // }
     if(snapshot.child("choice").exists() && snapshot.val().numOfPlayers !== playerNum) {
       opponentChoice = snapshot.val().choice;
-      console.log("opponentChoice", opponentChoice);
+      console.log("p1s opponentChoice", opponentChoice);
     }
   });
 
   player2.on("value", function(snapshot) {
     if(snapshot.child("playerName").exists()) {
+      //$("#p1Player").html("Player 1: " + player1.playerName);
       $("#p2Player").html("Player 2: " + playerName);
+      waitingForPlayer(playerCount);
     }
     if(snapshot.child("losses").exists()) {
       $("#p2Lose").html(snapshot.val().losses);
+      p2Lose = snapshot.val().losses;
+      console.log("p1 losses", p2Lose);
     }
     if(snapshot.child("wins").exists()) {
       $("#p2Wins").html(snapshot.val().wins);
+      p2Wins = snapshot.val().wins;
+      console.log("p2 wins", p2Wins);
     }
     // if(snapshot.child("choice").exists()) {
     //   $("#p1Choice").html(snapshot.val().losses);
     // }
     if(snapshot.child("choice").exists() && snapshot.val().numOfPlayers !== playerNum) {
       opponentChoice = snapshot.val().choice;
-      console.log("opponentChoice", opponentChoice);
+      console.log("p2s opponentChoice", opponentChoice);
     }
   });
-    // database.ref().on("child_added", function(childSnapshot) {
-
-   	//   console.log("snapshot", childSnapshot.val());
-   	//   console.log("Player "+ playerCount, childSnapshot.val().numOfPlayers);
-
-    //   //if playerCount == 1 or 2
-    //   if(playerCount == 1){
-    //     waitingForPlayer(playerCount);
-    //     $(".yourTurn").hide();
-    //     $(".waitingTurn").hide();
-
-    //   }else if(playerCount == 2){
-
-    //     waitingForPlayer(playerCount);
-
-    //   }
-
-      
-      
-    //Handles the errors
-    // }, function(errorObject) {
-    //  	console.log("The read failed: " + errorObject.code);
-    // });
-
 
 
 //-------------------------------------------------------------
@@ -199,32 +213,41 @@ function beforeGame() {
 function waitingForPlayer(player) {
   //display none rock, paper, sissors
   //display none wins & losses
-  $("#p"+ playerNum +"Player").html("Player 1: " + playerName);
-  $("#p"+ playerCount +"Waiting").hide();
-  playerPanel(playerCount);
+  //$("#p"+ player +"Player").html("Player 1: " + playerName);
+  $("#p"+ player +"Waiting").hide();
+  playerShowPanel(playerNum);
 };
 
-function playerPanel(player) {
-    rpsDisplay(player);
+function playerShowPanel(player) {
+    rpsShowDisplay(player);
     //display none wins & losses
     $("#p"+ player +"WinLose").show();
     //display non results panel
-    $(".yourTurn").show();
+    if(player == 1){
+      $(".waitingTurn").show();
+    }
+    else if(player == 2){
+      $(".yourTurn").show();
+      //whos turn, player1 or player2's turn
+    }
 }
 
-function rpsDisplay(player) {
+function rpsShowDisplay(player) {
   //display rock, paper, sissors for player
   $(".p"+ player +"rock").show();
   $(".p"+ player +"paper").show();
   $(".p"+ player +"sissors").show();
 }
 
-
-function playerTurn() {
+function choiceResults() {
 
 }
 
-function playerWait() {
+function winner() {
+
+}
+
+function loser() {
 
 }
 
